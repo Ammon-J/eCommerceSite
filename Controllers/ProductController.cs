8e387fde4848058ef128fd083eefc512696fa5d6
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-08-03 10:00:53 -0700

    Issue#6 product listing (#8)
    * Created product listing page
    
    * Refactored catalog to use model properties as colomn headers

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that lists all the products
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            // Get all products form the db
+            List<Product> products = _context.Products.ToList();
+
+            // Send the list of products to the view to be displayed0
+            return View(products);
+        }
+    }
+}
