8d3de4a47dc7db604932a00a1bb47bc50b4fd006
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-08-04 21:27:11 -0700

    Issue#5 add product (#12)
    * Add Product is implemented
    
    * Added styling to the Catalog page
    
    * Added styling to the add product form
    
    *Changed the validation format

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -26,8 +26,37 @@ namespace eCommerceSite.Controllers
             // Get all products form the db
             List<Product> products = _context.Products.ToList();
 
-            // Send the list of products to the view to be displayed0
+            // Send the list of products to the view to be displayed
             return View(products);
+        }
+
+        // Page for adding a new item
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // After all the inforamation has been entered and add it to the database
+        [HttpPost]
+        public IActionResult Add(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                // Add the entered product info to the database
+                _context.Products.Add(p);
+                _context.SaveChanges();
+                // Holy cow that was easy! I can do this all day
+
+                // TempData will not get deleted when you redirect to another page.
+                // Use TempData to display a success message.
+                TempData["Message"] = $"{p.Title} was added seccessfully";
+
+                // Redirect back to the catalog
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
