d1b426a3c5693feff1955d452fb99cdd3ceffa8a
Ammon Johnson <ammonsawesome@gmail.com>
2021-08-13 17:48:43 -0700

    Edit product implemented

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -62,6 +62,30 @@ namespace eCommerceSite.Controllers
             return View();
         }
 
-        //public async Task<IActionResult> 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // Get product with corrisponding id
+            Product p =
+                await (from prod in _context.Products
+                 where prod.ProductId == id
+                 select prod).SingleAsync();
+            // pass product to view
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                ViewData["Updated"] = "Product updated successfully";
+            }
+
+            return View(p);
+        }
     }
 }
