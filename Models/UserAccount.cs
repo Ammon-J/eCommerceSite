05d59e44e5d1cc2818fc51e32030fae28252c977
Ammon Johnson <ammonsawesome@gmail.com>
2021-08-19 21:58:51 -0700

    Creat RegisterViewModel

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -19,4 +19,27 @@ namespace eCommerceSite.Models
 
         public DateTime? DateOfBirth { get; set; }
     }
+
+    public class RegisterViewdel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        // This will make it so the email has to match the orgi email
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        // This makes the text box a password format
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        // This will only show the date and not the time
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
 }
