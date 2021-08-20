ef005e0e961fcee397dab2224edb9e8b57c10b20
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-08-20 14:52:03 -0700

    Issue#27 allow user to log in (#29)
    * Created a user account model
    
    * Added UserAccount to DB using code first migration
    
    * Creat RegisterViewModel
    
    * Created the Rigister page
    
    * Added register page

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public UserController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if(ModelState.IsValid)
+            {
+                // Map data to user account instances
+                UserAccount acc = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    Username = reg.Username
+                };
+
+                // Add the user acount to the db
+                await _context.UserAccounts.AddAsync(acc);
+                await _context.SaveChangesAsync();
+
+                // Redirect to homepage if everything is valid
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(reg);
+        }
+    }
+}
---
+++ Models/UserAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        // Set the maximum length for the email
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        // This will make it so the email has to match the orgi email
+        [Compare(nameof(Email))]
+        [Display(Name ="Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        // This makes the text box a password format
+        [DataType(DataType.Password)]
+        // Set the maximum and the minimum length for the password
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between {2} and {1}")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        // This will only show the date and not the time
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+    }
+}
