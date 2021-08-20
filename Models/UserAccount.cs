2fa9e5bab7f9ef56da30b9885841728cf108e7c7
Ammon Johnson <ammonsawesome@gmail.com>
2021-08-19 22:27:14 -0700

    Created the Rigister page

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -23,19 +23,26 @@ namespace eCommerceSite.Models
     public class RegisterViewdel
     {
         [Required]
+        [EmailAddress]
+        // Set the maximum length for the email
+        [StringLength(200)]
         public string Email { get; set; }
 
         // This will make it so the email has to match the orgi email
         [Compare(nameof(Email))]
+        [Display(Name ="Confirm Email")]
         public string ConfirmEmail { get; set; }
 
         [Required]
         // This makes the text box a password format
         [DataType(DataType.Password)]
+        // Set the maximum and the minimum length for the password
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between {2} and {1}")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
         // This will only show the date and not the time
