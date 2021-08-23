e62c902cdde9ea1f09b50cc4e221bc99b583a1d5
Ammon Johnson <ammonsawesome@gmail.com>
2021-08-23 12:57:32 -0700

    Got the login page to show error message whith bad username or password

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,9 +48,37 @@ namespace eCommerceSite.Controllers
             return View(reg);
         }
 
-        public async Task<IActionResult> Login()
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return View();
+            }
+            // Gets the mathing account from the db
+            UserAccount acc =
+                await (from u in _context.UserAccounts
+                 where (u.Username == model.UsernameOrEmail
+                     || u.Email == model.UsernameOrEmail)
+                     && u.Password == model.Password
+                 select u).SingleOrDefaultAsync();
+
+            if(acc == null)
+            {
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Account was not found with the given Username and Password");
+
+                // No account found
+                return View(model);
+            }
+
+            // Login 
+            return RedirectToAction("Index", "Home");
         }
     }
 }
