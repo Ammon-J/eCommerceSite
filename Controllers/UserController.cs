d35cef3f3dcd4a8f81e8c31aee48480594d0666e
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-08-23 22:34:07 -0700

    Issue#30 allow user to sign in (#31)
    * Created login page
    
    * Got the login page to show error message whith bad username or password
    
    * Enabled Sessions
    
    * Add note about where ot use session on Startup
    
    * Added session managment for log in page and Registered HttpService  in layout page
    
    * Added username greeting to navigation whern logged in

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,6 +47,48 @@ namespace eCommerceSite.Controllers
             }
 
             return View(reg);
+        }
+
+        public IActionResult Login()
+        {
+            // Chack if user is already logged in
+            if(HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return View();
+            }
+            // Gets the mathing account from the db
+            UserAccount acc =
+                await (from u in _context.UserAccounts
+                 where (u.Username == model.UsernameOrEmail
+                     || u.Email == model.UsernameOrEmail)
+                     && u.Password == model.Password
+                 select u).SingleOrDefaultAsync();
+
+            if(acc == null)
+            {
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Account was not found with the given Username and Password");
+
+                // No account found
+                return View(model);
+            }
+
+            // Login 
+            // Create a session for the user
+            HttpContext.Session.SetInt32("UserId", acc.UserId);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
