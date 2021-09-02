e03d96504d92c469f21afb4641b422bb0503f90f
Ammon Johnson <ammonsawesome@gmail.com>
2021-09-02 15:38:38 -0700

    Refactored cart controller

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -24,47 +24,16 @@ namespace eCommerceSite.Controllers
 
         public async Task<IActionResult> Add(int id) // This is the is to add to the cart
         {
-            // Cookie key
-            const string cartCookie = "CartCookie";
-
             Product p = await ProductDB.GetProductAsync(_context, id);
 
-            // Get existing cart items
-            string existingItems = _httpContext.HttpContext.Request.Cookies[cartCookie];
-
-            List<Product> cartProducts = new List<Product>();
-            if(existingItems != null)
-            {
-                // Get all the products in the cookie and return it in a list
-                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
-            }
-
-            // Add current products to existing cart
-            cartProducts.Add(p);
-
-            // Add products in a cookie
-            string data = JsonConvert.SerializeObject(cartProducts);
-
-            // Set up the cookie options
-            CookieOptions options = new CookieOptions()
-            {
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true
-            };
-
-            _httpContext.HttpContext.Response.Cookies.Append(cartCookie, data, options);
+            CookieHelper.addProductToCart(p, _httpContext);
 
             return RedirectToAction("Index", "Product");
         }
 
         public IActionResult Summary()
         {
-            // Display all the products in the shopping cart
-            string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
-
-            List<Product> cartProducts = JsonConvert.DeserializeObject<List<Product>>(cookieData);
-            return View(cartProducts);
+            return View(CookieHelper.GetCartProducts(_httpContext));
         }
     }
 }
