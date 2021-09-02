0d947bdeb21ef3ec94f4652f3c8894c8cf630644
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-09-02 15:40:56 -0700

    Issue#28 add shopping cart (#38)
    * Added shopping cart link to navigation and add to cart button for each product
    
    * Refactored get single product db query
    
    * A single product can now be added to the cart
    
    * Able to store multiple products in the cart
    
    * Created basic summary page. Does not work with empty cookie
    
    * Refactored cart controller
    
    * Added a message for an empty cart on the summary page

---
+++ Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        public async Task<IActionResult> Add(int id) // This is the is to add to the cart
+        {
+            Product p = await ProductDB.GetProductAsync(_context, id);
+
+            CookieHelper.addProductToCart(p, _httpContext);
+
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            return View(CookieHelper.GetCartProducts(_httpContext));
+        }
+    }
+}
---
+++ Models/CookieHelper.cs
@@ -0,0 +1,60 @@
+﻿using eCommerceSite.Data;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public static class CookieHelper
+    {
+
+        // Cookie key
+        const string cartCookie = "CartCookie";
+        /// <summary>
+        /// Returns the current list of products
+        /// </summary>
+        /// <param name="http"></param>
+        /// <returns>An empty list if cart is empty</returns>
+        public static List<Product> GetCartProducts(IHttpContextAccessor http)
+        {
+
+            // Get existing cart items
+            string existingItems = http.HttpContext.Request.Cookies[cartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+            if (existingItems != null)
+            {
+                // Get all the products in the cookie and return it in a list
+                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+
+            return cartProducts;
+        }
+
+        public static void addProductToCart(Product p, IHttpContextAccessor http)
+        {
+            List<Product> cartProducts = GetCartProducts(http);
+            cartProducts.Add(p);
+
+            string data = JsonConvert.SerializeObject(cartProducts);
+
+            // Set up the cookie options
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(cartCookie, data, options);
+        }
+
+        public static int GetNumberOfCartItems(IHttpContextAccessor http)
+        {
+
+        }
+    }
+}
